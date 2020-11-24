6f57b22a0475c90f0ce12e5fa2f2cfc723a09ad2
Teddyzander <edwardalexandersmall@hotmail.co.uk>
2020-11-24 11:25:25 +0000

    Added element accessors and size accessor functions

--- ODE_solver/Project1/mvector.h
+++ ODE_solver/Project1/mvector.h
@@ -3,7 +3,7 @@
 
 #include <vector> // bring in vector library
 
-//Class that represents mathematical vector
+// Class that represents mathematical vector
 class MVector 
 {
 public:
@@ -12,6 +12,17 @@ public:
 	explicit MVector(int n) : v(n) {}
 	MVector(int n, double x) : v(n, x) {}
 	MVector(std::initializer_list<double> l) : v(l) {}
+
+	// element accessors 
+
+	// access mutable element at index "index" (read and write)
+	double &operator[](int index) { return v[index]; }
+
+	// access immutable element at index "index" (read only)
+	double operator[](int index) const { return v[index];  }
+
+	// get number of elements in vector
+	int size() const { return v.size(); }
 
 private:
 	std::vector<double> v;
