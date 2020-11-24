34464e9809924b7a49786215f50039eea5c27e2b
Teddyzander <edwardalexandersmall@hotmail.co.uk>
2020-11-24 11:14:49 +0000

    Add private member variable vector of doubles "v"
    Add contructors for different scenarios

--- ODE_solver/Project1/mvector.h
+++ ODE_solver/Project1/mvector.h
@@ -4,8 +4,17 @@
 #include <vector> // bring in vector library
 
 //Class that represents mathematical vector
-class MVector {
+class MVector 
+{
+public:
+	// constructors
+	MVector() {}
+	explicit MVector(int n) : v(n) {}
+	MVector(int n, double x) : v(n, x) {}
+	MVector(std::initializer_list<double> l) : v(l) {}
 
+private:
+	std::vector<double> v;
 };
 
 #endif
