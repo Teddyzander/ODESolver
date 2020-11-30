6a691d2c5ae91cf2a98666a86d3e2ca0c217a35a
Teddyzander <edwardalexandersmall@hotmail.co.uk>
2020-11-30 16:08:31 +0000

    Runge kutta implemented, with optional file save

--- ODE_solver/Project1/EulerSolve.h
+++ ODE_solver/Project1/EulerSolve.h
@@ -2,6 +2,7 @@
 #define EULERSOLVE_H
 
 #include "mFunction.h"
+#include <string>
 #include<fstream>
 
 struct SolverOutput {
@@ -20,13 +21,15 @@ a (double): value of x to start estimates at
 b (double): value of x to end estimates at
 y (MVector): vector to hold estimated values of y at each step
 f (MFunction): function(s) to evaluate
+save (bool): whether or not to save outputs to file
 
 OUTPUTS:
 
-error (int): An error code to describe problems the analysis may have encountered
+result (SolverOutput): A struct holding estimated values for Y at x and an error code
 */
-SolverOutput EulerSolver(int steps, double a, double b, MVector& y, MFunction& f)
+SolverOutput EulerSolve(int steps, double a, double b, MVector& y, MFunction& f, bool save = false)
 {
+	std::ofstream myFile;
 	// Struct to hold return values and error value
 	SolverOutput result;
 	// declare the initial starting point for x
@@ -34,17 +37,102 @@ SolverOutput EulerSolver(int steps, double a, double b, MVector& y, MFunction& f
 	// calculate the size of each step
 	double h = abs(a - b) / steps;
 
+	if (save == true) { myFile.open("eulerSolver" + std::to_string(steps) + ".txt"); }
+
 	// loop over
 	for (int i = 0; i < steps - 1; i++)
 	{
 		x = a + i * h;
 
+		if (save == true)
+		{
+			myFile << "step: " << i << "\t" << x << "\t" << y << std::endl;
+		}
+
 		y = y + h * f(x, y);
 	}
 
 	x = b;
+
+	if (save == true)
+	{
+		myFile << "step: " << steps << "\t" << x << "\t" << y << std::endl;
+	}
+
+
+	myFile.close();
+
 	std::cout << "x: " << x << "\ny: " << y << std::endl;
-	
+
+	result.x = x;
+	result.y = y;
+	result.error = 0;
+
+	return result;
+}
+
+/*
+Function to estimate the value(s) of a function(s) at x = b
+
+INPUTS:
+
+steps (int): integer for number of steps wanted between a and b
+a (double): value of x to start estimates at
+b (double): value of x to end estimates at
+y (MVector): vector to hold estimated values of y at each step
+f (MFunction): function(s) to evaluate
+save (bool): whether or not to save outputs to file
+
+OUTPUTS:
+
+result (SolverOutput): A struct holding estimated values for Y at x and an error code
+*/
+
+SolverOutput RungeKutta(int steps, double a, double b, MVector& y, MFunction& f, bool save = false)
+{
+	std::ofstream myFile;
+	// Struct to hold return values and error value
+	SolverOutput result;
+	// declare the initial starting point for x
+	double x;
+	// calculate the size of each step
+	double h = abs(a - b) / steps;
+
+	MVector k1, k2, k3, k4;
+
+	if (save == true) { myFile.open("rungeKutta" + std::to_string(steps) + ".txt");	}
+
+	// loop over
+	for (int i = 0; i < steps - 1; i++)
+	{
+		x = a + i * h;
+
+		if (save == true)
+		{
+			myFile << "step: " << i << "\t" << x << "\t" << y << std::endl;
+		}
+
+		k1 = f(x, y);
+		k2 = f(x + h / 2, y + (h / 2) * k1);
+		k3 = f(x + h / 2, y + (h / 2) * k2);
+		k4 = f(x + h, y + h * k3);
+
+		y = y + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4);
+		
+	}
+
+	x = b;
+
+	if (save == true)
+	{
+		myFile << "step: " << steps << "\t" << x << "\t" << y << std::endl;
+	}
+
+
+	myFile.close();
+
+	std::cout << "x: " << x << "\ny: " << y << std::endl;
+
 	result.x = x;
 	result.y = y;
 	result.error = 0;
--- ODE_solver/Project1/Main.cpp
+++ ODE_solver/Project1/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <fstream>
 
 #include "EulerSolve.h"
 #include "functionF2.h"
@@ -7,9 +8,16 @@
 
 int main()
 {
-	MVector y({ 0, 1 });
 	FunctionF2 f;
-	EulerSolver(160, 0, 1, y, f);
+	SolverOutput data;
+
+	for (int i = 10; i < 1281; i *= 2)
+	{
+		MVector y1({ 0, 1 });
+		data = EulerSolve(i, 0, 1, y1, f, true);
+		MVector y2({ 0, 1 });
+		data = RungeKutta(i, 0, 1, y2, f, true);
+	}
 
 	return 0;
 }
